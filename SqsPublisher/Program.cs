cf634c189717cb458a719763052485468eff0679
MohammadMobasher <m.mobasher.z@gmail.com>
2024-03-25 14:59:26 +0330

    init

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.Text.Json;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "m.mobasher.z@gmail.com",
+    GitHubUserName = "MohammadMobasher",
+    FullName = "Mohammad Mobasher",
+    DateOfBirth = new DateTime(year: 1993, month: 01, day: 01),
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customer");
+
+var response = await sqsClient.SendMessageAsync(new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue> 
+    {
+        {
+            "MessageType", new MessageAttributeValue 
+            {
+                DataType = "string",
+                StringValue = nameof(CustomerCreated),
+                
+            } 
+        }
+    },
+});
+
