6f7409e7dbe43e7bc745cd7ee5309a280f4f2839
MohammadMobasher <m.mobasher.z@gmail.com>
2024-03-27 21:20:11 +0330

    create a simple snsPublisher project

---
+++ SNS/SnsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+using System.Text.Json;
+
+var customerCreated = new CustomerCreated
+{
+    Email = "m.mobasher.z@gmail.com",
+    FullName = "Mohammad Moabasher",
+    DateOfBirth = new DateTime(1993, 1, 1),
+    GitHubUserName = "MohammadMobasher",
+    Id = Guid.NewGuid(),
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+var topicArnResponse = await snsClient.FindTopicAsync("customer");
+
+var publishRequest = new PublishRequest
+{
+    TargetArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customerCreated),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "string",
+                StringValue = nameof(CustomerCreated),
+
+            }
+        }
+    },
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
